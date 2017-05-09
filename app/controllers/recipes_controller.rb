1b07f0982bd6a566ba121b139417942911cb268c
emilyjf <flanagan.emily.j@gmail.com>
2017-05-08 19:05:38 -0500

    initial commit

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,52 @@
+class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+    recipe_id = params[:id]
+    @recipe = Recipe.find_by(id: recipe_id)
+  end
+
+  def new
+    
+  end
+
+  def create
+    recipe = Recipe.new(
+                        title: params[:title],
+                        chef: params[:chef],
+                        ingredients: params[:ingredients],
+                        directions: params[:directions]
+                        )
+    recipe.save
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    recipe = Recipe.find(params[:id])
+    recipe.assign_attributes(
+                             title: params[:title],
+                             chef: params[:chef],
+                             ingredients: params[:ingredients],
+                             directions: params[:directions]
+                             )
+    recipe.save
+  end
+
+  def destroy
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+  end
+end
+
+
+
+
+
+
+
+
